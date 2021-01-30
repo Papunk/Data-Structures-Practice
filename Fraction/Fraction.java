378eede520d49ae7736832e401061998a2ac34a8
Pedro <pedrocpt2001@gmail.com>
2021-01-30 13:50:08 -0400

    added fraction implementation

--- Fraction/Fraction.java
+++ Fraction/Fraction.java
@@ -1,5 +1,53 @@
 package Fraction;
 
 public class Fraction {
+    private int a;
+    private int b;
+
+    public Fraction(int a, int b) {
+        if (b == 0) return;
+        this.a = a;
+        this.b = b;
+    }
+
+
+    public int getNumerator() {
+        return a;
+    }
+
+    public int getDenominator() {
+        return b;
+    }
+
+    public double computeFraction() {
+        return (double) a / b;
+    }
+
+    public Fraction getReciprocal() {
+        return new Fraction(this.b, this.a);
+    }
+
+
+    public static Fraction add(Fraction f1, Fraction f2) {
+        int newA = (f1.getNumerator() * f2.getDenominator()) + (f2.getNumerator() * f1.getDenominator());
+        int newB = (f1.getDenominator() * f2.getDenominator());
+        return new Fraction(newA, newB);
+    }
+
+    public static Fraction subtract(Fraction f1, Fraction f2) {
+        int newA = (f1.getNumerator() * f2.getDenominator()) - (f2.getNumerator() * f1.getDenominator());
+        int newB = (f1.getDenominator() * f2.getDenominator());
+        return new Fraction(newA, newB);
+    }
+
+    public static Fraction multiply(Fraction f1, Fraction f2) {
+        int newA = f1.getNumerator() * f2.getNumerator();
+        int newB = f1.getDenominator() * f2.getDenominator();
+        return new Fraction(newA, newB);
+    }
+
+    public static Fraction divide(Fraction f1, Fraction f2) {
+        return multiply(f1, f2.getReciprocal());
+    }
 
 }
---
+++ Fraction/MixedNumber.java
@@ -0,0 +1,7 @@
+package Fraction;
+
+public class MixedNumber extends Fraction{
+    public MixedNumber(int a, int b) {
+        super(a, b);
+    }
+}
