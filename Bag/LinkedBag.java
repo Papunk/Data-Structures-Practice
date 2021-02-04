8ce06cfe9a8077f879fc9fe894b19fad219e7741
Pedro <pedrocpt2001@gmail.com>
2021-02-04 14:20:16 -0400

    added internal node class

--- Bag/LinkedBag.java
+++ Bag/LinkedBag.java
@@ -1,2 +1,77 @@
-package Bag;public class LinkedBag {
+package Bag;
+
+public class LinkedBag<T> implements Bag<T> {
+
+    private Node firstNode;
+    private int numOfElems;
+
+    public LinkedBag() {
+        firstNode = null;
+        numOfElems = 0;
+    }
+
+    @Override
+    public int getCurrentSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains() {
+        return false;
+    }
+
+    @Override
+    public boolean hasDuplicates() {
+        return false;
+    }
+
+    @Override
+    public void add(T newEntry) {
+
+    }
+
+    @Override
+    public T remove() {
+        return null;
+    }
+
+    @Override
+    public T remove(T elem) {
+        return null;
+    }
+
+    @Override
+    public int getFrecuencyOf(T elem) {
+        return 0;
+    }
+
+    @Override
+    public void removeAll() {
+
+    }
+
+    @Override
+    public T[] toArray() {
+        return null;
+    }
+
+
+    private class Node {
+        private T data;
+        private Node next;
+
+        private Node(T data) {
+            this(data, null);
+        }
+
+        private Node(T data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
 }
