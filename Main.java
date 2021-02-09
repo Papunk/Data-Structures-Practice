557734194e3808202e88b87f12fe9566f1aaf4db
Pedro <pedrocpt2001@gmail.com>
2021-02-08 23:11:00 -0400

    Cleaned up main function

--- Main.java
+++ Main.java
@@ -7,15 +7,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Bag<Integer> b = new LinkedBag<Integer>();
-
-        b.add(3);
-        b.add(55);
-        b.add(123);
-
-        System.out.println(Arrays.toString(b.toArray()));
-        b.removeAll();
-        System.out.println(Arrays.toString(b.toArray()));
-
     }
 }
