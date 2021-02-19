8d1aac73346a01f55c4bb1d65babf39fb5049e87
Pedro <pedrocpt2001@gmail.com>
2021-02-18 22:34:44 -0400

    Finished backbone of SimpleList class

--- SimpleList/SimpleList.java
+++ SimpleList/SimpleList.java
@@ -2,9 +2,16 @@ package SimpleList;
 
 public interface SimpleList<T> {
 
+    void add();
+
     T getFirst();
     T getLast();
 
     T removeFirst();
     T removeLast();
+    T[] removeAll();
+    T remove(T elem);
+    T removeAll(T elem);
+
+    T[] toArray();
 }
