a2deb7b88c094d5a31d126ae8c8d079e5a8e46cc
Pedro <pedrocpt2001@gmail.com>
2021-02-22 08:48:14 -0400

    Started SLLSimpleList implementation

--- SimpleList/SLLSimpleList.java
+++ SimpleList/SLLSimpleList.java
@@ -1,20 +1,36 @@
 package SimpleList;
 
+import java.util.EnumMap;
+
 public class SLLSimpleList<T> implements SimpleList<T> {
+
+    private Node first;
+    private Node last;
+    private int numOfElems;
+
+    public SLLSimpleList() {
+        first = null;
+        last = null;
+        numOfElems = 0;
+    }
 
     @Override
     public void add(T elem) {
-
+        first = new Node(elem, first);
+        if (numOfElems == 0) {
+            last = first;
+        }
+        numOfElems++;
     }
 
     @Override
     public T getFirst() {
-        return null;
+        return first.data;
     }
 
     @Override
     public T getLast() {
-        return null;
+        return last.data;
     }
 
     @Override
@@ -45,5 +61,19 @@ public class SLLSimpleList<T> implements SimpleList<T> {
     @Override
     public T[] toArray() {
         return null;
+    }
+
+    private class Node {
+        private T data;
+        private Node next;
+
+        private Node(T data) {
+            this(data, null);
+        }
+
+        private Node(T data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
     }
 }
