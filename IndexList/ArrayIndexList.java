55d4879300c6a0b3c36b2608faa9b84fa2fb3fd0
Pedro <pedrocpt2001@gmail.com>
2021-03-05 17:19:50 -0400

    Wrote methods into IndexList implementations

--- IndexList/ArrayIndexList.java
+++ IndexList/ArrayIndexList.java
@@ -1,4 +1,43 @@
 package IndexList;
 
 public class ArrayIndexList implements IndexList{
+    @Override
+    public Object getElementAt(int index) {
+        return null;
+    }
+
+    @Override
+    public void append(Object element) {
+
+    }
+
+    @Override
+    public void insert(Object element, int atIndex) {
+
+    }
+
+    @Override
+    public Object removeFirstInstanceOf(Object element) {
+        return null;
+    }
+
+    @Override
+    public Object removeAllInstancesOf(Object element) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Boolean contains(Object element) {
+        return null;
+    }
+
+    @Override
+    public int getLength() {
+        return 0;
+    }
 }
