6c431b8077675b0bbcb76a93ed5b02b45f39e01a
Pedro <pedrocpt2001@gmail.com>
2021-03-18 22:00:46 -0400

    Completed LinkedStack

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ import Bag.LinkedBag;
 import SimpleList.ArraySimpleList;
 import Stack.Stack;
 import Stack.ArrayStack;
-
+import Stack.LinkedStack;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        Stack<Integer> a = new ArrayStack<Integer>();
+        Stack<Integer> a = new LinkedStack<Integer>();
         for (int i = 0; i < 100; i++) {
             a.push(i);
         }
--- Stack/LinkedStack.java
+++ Stack/LinkedStack.java
@@ -1,4 +1,38 @@
 package Stack;
 
-public class LinkedStack {
+import Node.GenericNode;
+
+import java.util.EmptyStackException;
+
+public class LinkedStack<T> implements Stack<T> {
+
+    private GenericNode<T> head;
+
+    public LinkedStack() {
+        head = null;
+    }
+
+    @Override
+    public void push(T element) {
+        head = new GenericNode(element, head);
+    }
+
+    @Override
+    public T peek() {
+        if (isEmpty()) throw new EmptyStackException();
+        return head.data;
+    }
+
+    @Override
+    public T pop() {
+        if (isEmpty()) throw new EmptyStackException();
+        T etr = head.data;
+        head = head.next;
+        return etr;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
 }
