dd58f6afbb46c172ec0fa859d51268d5d1adcbe7
Pedro <pedrocpt2001@gmail.com>
2021-04-08 15:01:03 -0400

    Added skeleton for recursive search

--- Recursion/RecursiveSearch.java
+++ Recursion/RecursiveSearch.java
@@ -1,4 +1,14 @@
 package Recursion;
 
-public class RecursiveSearch {
+public class RecursiveSearch<T> {
+
+    private T[] arr;
+
+    public RecursiveSearch(T[] arr) {
+        this.arr = arr;
+    }
+
+    public int searchFor(T elem) {
+        return 0;
+    }
 }
