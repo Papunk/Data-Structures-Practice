66a9f63a4d000eeab0de6aee487b2ba377ac4b80
Pedro <pedrocpt2001@gmail.com>
2021-04-27 14:20:17 -0400

    Added Tree interface

--- Main.java
+++ Main.java
@@ -19,7 +19,7 @@ public class Main {
     public static void main(String[] args) {
         IntegerArrayListSortedList l = new IntegerArrayListSortedList();
         Random r = new Random();
-
+        
         for (int i = 0; i < 20; i++) {
             int n = r.nextInt(100);
             System.out.println(n);
